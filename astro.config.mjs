018ed182a9b670139bf0860b01427c3596ed2ddd
LucMuhizi <muhi0019@algonquinlive.com>
2023-09-06 12:34:15 +0300

    fixed the site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://haze-youth.vercel.app/",
+  site: "https://hazeyouth.net",
   integrations: [
     tailwind(),
     image({
